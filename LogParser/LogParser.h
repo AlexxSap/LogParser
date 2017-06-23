6574d2bd71dc9d11068157a80faa2774890c6fac
Александр Сапожников (Aleksandr Sapozhnikov) <alexxandrsap@mail.ru>
2017-06-23 22:03:26 +0300

    убрал лишний код

--- LogParser/LogParser.h
+++ LogParser/LogParser.h
@@ -7,31 +7,11 @@
 
 class LogParser
 {
-private:
-    struct TopLine
-    {
-        TopLine(const std::string &name,
-                const unsigned &count)
-            : name(name),
-              count(count) {}
 
-        bool operator < (const TopLine &other)
-        {
-            if(count == other.count)
-            {
-                return name < other.name;
-            }
-            return count < other.count;
-        }
-
-        std::string name;
-        unsigned count;
-    };
 public:
     LogParser(const std::string &logFileName,
               const std::string &resultFileName,
               const int &topCount = 5);
-
     bool make();
 
 private:
@@ -45,7 +25,7 @@ private:
     std::string _resultFileName;
     unsigned _topCount;
 
-    std::unordered_set<std::string> _urls_;
+    std::unordered_set<std::string> _urls;
 
     std::map<std::string, unsigned> _topDomains;
     std::map<std::string, unsigned> _topPaths;
